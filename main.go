883732a7cd049def472ea09edd6d0ed55d7dfa98
刘宏 <2757634627@qq.com>
2020-09-27 00:07:10 +0800

    https://github.com/wxsmr/xinBeegoDemo.git

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "xinBeegoDemo/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"xinBeegoDemo/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
